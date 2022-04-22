e058dca693fcb88580098c9c0fd0de79c11a8e8f
j.spijker@ultimaker.com <jelle spijker>
2022-04-22 15:29:53 +0200

    Created a Github workflow
    This should only run and export the recipes which are changed.
    on pushes against main, dev and CURA-**** branches or pr's
    against main and dev.
    
    Pushes against:
    - main will result in a new package in the ultimaker/stable channel
    - dev will result in a new package in the ultimaker/testing channel
    - CURA-**** will create a temporary ultimaker/CURA-**** channel
    
    The idea is to work on a CURA branch, then push to dev. If things work
    correctly push to main.
    
    Contributes to CURA-9177

--- scripts/export_all.py
+++ scripts/export_all.py
@@ -5,11 +5,28 @@ import subprocess
 from pathlib import Path
 
 if __name__ == "__main__":
-    recipes_paths = Path(__file__).absolute().parents[1].joinpath("recipes").glob("**/conandata.yml")
-    for recipe_path in recipes_paths:
-        recipe_name = recipe_path.parts[-2]
-        export_path = recipe_path.parent
-        with open(recipe_path, "r") as f:
+    user = sys.argv[1]
+    ref_name = sys.argv[2]
+
+    changed_recipes = []
+    for file in sys.argv[3:]:
+        file_path = Path(file)
+        if "recipes" in file_path.parts:
+            changed_recipes.append(Path("/".join(file_path.parts[:file_path.parts.index("recipes") + 2])))
+
+    if ref_name == "main":
+        channel = "stable"
+    elif ref_name == "dev":
+        channel = "testing"
+    else:
+        channel = ref_name[:9] if ref_name.startswith("CURA-") and len(ref_name) >= 9 else ref_name
+
+    for recipe_path in changed_recipes:
+        recipe_name = recipe_path.parts[-1]
+        conandata_path = recipe_path.joinpath("conandata.yml")
+        if not conandata_path.exists():
+            continue
+        with open(conandata_path, "r") as f:
             parsed_yaml = yaml.safe_load(f)
             for recipe_version in parsed_yaml["sources"].keys():
-                subprocess.run(f"conan export {export_path} {recipe_name}/{recipe_version}@{sys.argv[1]}/{sys.argv[2]}", shell = True)
+                subprocess.run(f"conan export {recipe_path} {recipe_name}/{recipe_version}@{user}/{channel}", shell = True)
