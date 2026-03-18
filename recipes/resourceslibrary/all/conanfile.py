0abd05f7e2d72c534817bbb708f847327d8a6e81
Erwan MATHIEU <erwan.mathieu@ultimaker.com>
2026-03-18 11:19:24 +0100

    Add resources library package type

---
+++ recipes/resourceslibrary/all/conanfile.py
@@ -0,0 +1,28 @@
+import hashlib
+import os
+
+from conan import ConanFile
+
+required_conan_version = ">=2.7.0"
+
+
+class ResourcesLibrary:
+    def package_id(self):
+        # Include all settings and options by default
+        super().package_id()
+
+        # Add a checksum of the source files to the package_id, so that any source file change will be considered for a rebuild
+        sources_checksum = hashlib.sha256()
+        for folder, _subfolders, files in os.walk(self.source_path):
+            for file_name in files:
+                file_path = os.path.join(folder, file_name)
+                with open(file_path, 'rb') as f:
+                    sources_checksum.update(f.read())
+
+        self.info.settings.append("source_checksum", sources_checksum.hexdigest())
+
+
+class PyReq(ConanFile):
+    name = "resourceslibrary"
+    description = "This is a base conan file description for packages that define resources to be appended to cura"
+    package_type = "python-require"
